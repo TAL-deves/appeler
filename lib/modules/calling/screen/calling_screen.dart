81e9e2593b1f519f090e870ca5a03a34a233ebca
appdevlopment <shimul6680@gmail.com>
2023-03-21 14:33:37 +0600

    works hard

--- lib/modules/calling/screen/calling_screen.dart
+++ lib/modules/calling/screen/calling_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 import '../../auth/api/auth_management.dart';
 import 'call_enum/call_enum.dart';
 
@@ -28,6 +29,28 @@ class _CallingScreenState extends State<CallingScreen> {
 
   StreamSubscription? _roomSubs;
 
+
+  final _localRenderer = RTCVideoRenderer();
+  //final _remoteRenderer = RTCVideoRenderer();
+  late MediaStream _localStream;
+
+  Future<MediaStream> get _getUserMediaStream async{
+    final mp = <String, dynamic>{
+      'audio': false,
+      'video': {
+        'facingMode': 'user'
+      }
+    };
+    final stream = await navigator.mediaDevices.getUserMedia(mp);
+    _localRenderer.srcObject = stream;
+    Future.delayed(const Duration(seconds: 1)).then((value){
+      setState(() {
+
+      });
+    });
+    return stream;
+  }
+
   void _initRoom(){
     _roomSubs = curRoom.snapshots().listen((event) {
       final data = event.data();
@@ -44,15 +67,38 @@ class _CallingScreenState extends State<CallingScreen> {
     _roomSubs?.cancel();
   }
 
+  Future<void> _initLocalRenderer() async{
+    await _localRenderer.initialize();
+    _localStream = await _getUserMediaStream;
+  }
+
+  Future<void> _disposeLocalRenderer() async{
+    _localRenderer.dispose();
+    _localStream.dispose();
+  }
+
+  Future<void> _initRemoteRenderer() async{
+
+  }
+
+  void _initRenderers() {
+    _initLocalRenderer();
+  }
+
+  void _disposeRenderers(){
+    _disposeLocalRenderer();
+  }
+
   @override
   void dispose() {
     _deleteRoomAndRecoverState();
+    _disposeRenderers();
     super.dispose();
   }
 
   @override
   void initState() {
-    //_answerCall();
+    _initRenderers();
     _initRoom();
     super.initState();
   }
@@ -62,6 +108,18 @@ class _CallingScreenState extends State<CallingScreen> {
     print('enum is: ${widget.callEnum}');
     return Scaffold(
       appBar: AppBar(title: const Text('Calling Screen')),
+      body: Column(
+        children: [
+          Flexible(
+            child: Container(
+              key: const Key('local'),
+              margin: const EdgeInsets.all(16),
+              decoration: const BoxDecoration(color: Colors.black),
+              child: RTCVideoView(_localRenderer, mirror: true),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
