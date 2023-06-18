0c5fe4062ede151d010c2d316c3c6149acce13b0
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-06-18 15:53:13 +0600

    main

--- lib/feature/presentation/pages/splash/activity.dart
+++ lib/feature/presentation/pages/splash/activity.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_andomie/core.dart';
+import 'package:flutter_androssy/widgets.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import '../../../../index.dart';
 
@@ -19,10 +21,19 @@ class SplashActivity extends StatelessWidget {
         titleAllCaps: true,
         titleExtraSize: 2,
         titleSize: 24,
+        duration: 2000000000,
         titleColor: AppColors.secondary,
         subtitle: AppInfo.description,
         logo: AppInfo.logo,
         logoColor: AppColors.primary,
+        bottom: TextView(
+          marginBottom: 40,
+          textAlign: TextAlign.center,
+          textColor: Colors.grey,
+          textSize: 12,
+          text:
+              'Powered by Tech Analytica Limited || Version ${locator<PackageInfo>().version}',
+        ),
         onRoute: (context) {
           locator<AuthHandler>().isSignIn().then((value) {
             if (value) {
