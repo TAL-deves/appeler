3c56b5d038ef862a7ff0b4c5812d7059b9d40169
appdevlopment <shimul6680@gmail.com>
2023-07-09 12:01:06 +0600

    web bug fixed

--- lib/core/constants/app_contents.dart
+++ lib/core/constants/app_contents.dart
@@ -1,8 +1,7 @@
 class AppContents {
   const AppContents._();
 
-  static const String prepareMeetingAvatar =
-      "https://assets.materialup.com/uploads/b78ca002-cd6c-4f84-befb-c09dd9261025/preview.png";
+  static const String prepareMeetingAvatar = "assets/images/avatar.png";
   static const String backgroundCover = "assets/images/background_primary.png";
   static const String backgroundCover1 = "assets/images/background_bubble_1.png";
   static const String backgroundCover2 = "assets/images/background_bubble_2.png";
